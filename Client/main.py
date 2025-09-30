2fa45cbf37f6779bb87afdba00ca5822c694a980
bryanbarakatsky <bryan.barakat.work@gmail.com>
2025-09-30 15:49:10 +0100

    CREATE: instantiated pet object in main

--- Client/main.py
+++ Client/main.py
@@ -1,9 +1,14 @@
-# """
-# main client script to demonstrate Pet and Item classes.
-# """
-#
-# def main():
-#     #code
-#
-# if __name__ == "__main__":
-#     main()
+"""
+main client script to demonstrate Pet and Item classes.
+"""
+
+from oopWorkshop.Pet.pet import Pet
+
+def main():
+    dog = Pet("Chop", 38, "dog")
+    dog.age = 49
+    print(dog.age)
+    print(dog.make_sound())
+
+if __name__ == "__main__":
+    main()
--- Pet/pet.py
+++ Pet/pet.py
@@ -1,46 +1,53 @@
 """
-    Defines the pet class with getters and setters
+    defines the pet class with getters and setters
 """
 
 class Pet:
     """
-        A class that represents a pet
+        class that represents a pet
     """
     def __init__(self, name: str, age: int, species: str):
         self._name = name
         self._age = age
         self._species = species
 
-    # Getters and Setters
-    def get_name(self) -> str:
-        """ Returns the name of the pet"""
+    #name
+    @property
+    def name(self) -> str:
+        """ returns the name of the pet """
         return self._name
 
-    def set_name(self, name: str) -> None:
-        """ Sets the name of the pet """
+    @name.setter
+    def name(self, name: str) -> None:
+        """ sets the name of the pet """
         if len(name) < 1:
             raise ValueError("Name cannot be empty")
         self._name = name
 
-    def get_age(self) -> int:
-        """ Returns the age of the pet"""
+    #age
+    @property
+    def age(self) -> int:
+        """ returns the age of the pet """
         return self._age
 
-    def set_age(self, age: int) -> None:
-        """ Sets the age of the pet """
+    @age.setter
+    def age(self, age: int) -> None:
+        """ sets the age of the pet """
         if age < 0:
             raise ValueError("Age cannot be negative")
         self._age = age
 
-    def get_species(self) -> str:
-        """ Returns the species of the pet"""
+    #species
+    @property
+    def species(self) -> str:
         return self._species
 
-    def set_species(self, species: str) -> None:
-        """ Sets the species of the pet """
+    @species.setter
+    def species(self, species: str) -> None:
+        """ sets the species of the pet """
         self._species = species
 
     def make_sound(self) -> str:
-        """ Makes the pet sound"""
-        sounds = {"dog" : "woof", "cat": "meow"}
+        """ makes the pet sound """
+        sounds = {"dog": "woof", "cat": "meow"}
         return sounds[self._species]
