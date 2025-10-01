5fda0052d47e205af3eb6d6cd4803282d7761d16
bryanbarakatsky <bryan.barakat.work@gmail.com>
2025-10-01 09:30:18 +0100

    CREATE: created a Subclass Dog that inherits from superclass Pet

--- Client/main.py
+++ Client/main.py
@@ -4,15 +4,22 @@ main client script to demonstrate Pet and Item classes.
 
 from oopWorkshop.Pet.pet import Pet
 from oopWorkshop.Pet.item import Item
+from oopWorkshop.Pet.dog import Dog
 
 def main():
     dog = Pet("Chop", 38, "dog")
     item = Item("Ball", 20)
     dog.age = 49
-    print(dog.age)
-    print(dog.make_sound())
-    print(item.get_name(), item.get_value())
+    # print(dog.age)
+    # print(dog.make_sound())
+    # print(item.get_name(), item.get_value())
+
+    my_dog = Dog(dog.name, dog.age, "Labrador")
+    print(my_dog.age)
+    print(my_dog.fetch("ball"))
+    print(my_dog.make_sound())
 
 if __name__ == "__main__":
     main()
+
 
---
+++ Pet/dog.py
@@ -0,0 +1,22 @@
+"""
+    defines the dog class
+"""
+from oopWorkshop.Pet.pet import Pet
+
+class Dog(Pet):
+
+    """
+        dog class inherits from Pet class and adds behaviour
+    """
+
+    def __init__(self, name: str, age: int, breed: str):
+        super().__init__(name, age, "Dog")
+        self.breed = breed
+
+    def make_sound(self) -> str:
+        """ makes dog make a sound (overrides method from superclass) """
+        return "Bark!!"
+
+    def fetch(self, item: str) -> str:
+        """ makes dog fetch item """
+        return f"{self.name}, go fetch the {item}"
